d9649522dd4045ff42c18a681165181abb58984f
mohammad sarahni <117571362+muhammadser1@users.noreply.github.com>
2025-03-12 22:22:27 +0200

    Update CreateIndividualLesson.jsx

--- my-app/src/Components/CreateIndividualLesson.jsx
+++ my-app/src/Components/CreateIndividualLesson.jsx
@@ -41,6 +41,8 @@ export default function CreateIndividualLesson() {
 
     // ✅ Convert Arabic hour labels to decimal values
     const hourOptions = {
+        "نصف ساعة": 0.5,
+        "ثلث ارباع الساعة": 0.75,
         "ساعة": 1,
         "ساعة وربع": 1.25,
         "ساعة ونصف": 1.5,
